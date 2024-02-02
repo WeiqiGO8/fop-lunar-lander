083c0c90f743f947054318820e79ab7b8e89dcf3
WeiqiGO8 <hannah.lingzhi@gmail.com>
2024-02-02 21:02:54 +0100

    made some changes in the plan
    tried to start implementing the new plan in the code

--- lunar-lander.js
+++ lunar-lander.js
@@ -1,53 +1,95 @@
+//varables
+let gameIsRunning = true;
+
+//arrays
 let values = [];
+let keyboard = [];
 
-for (let i = 0; i < 200; i++) {
-  let value = {
-    obstaclesX: 600,
-    vehicleY: 100,
-    velocity: 0.2,
-    acceleration: 0.09,
-    gameIsRunning: true,
-  };
+//create objects
+let value = {
+  x: 100,
+  y: 100,
+  obstacleX: 600,
+  vehicleY: 100,
+  velocity: 0.2,
+  acceleration: 0.09,
+  gameIsRunning: true,
+};
 
-  values.push(value);
-}
+values.push(value);
 
-//Step 0 - setup the canvas
+let key = {
+  arrowUp: 38,
+  arrowRight: 39,
+  arrowDown: 40,
+  arrowLeft: 37,
+  spacebar: 32,
+};
+
+keyboard.push(key);
+
+//setup the canvas
 function setUp() {
   createCanvas(600, 300);
   background(255, 255, 255);
 }
 
-//Step 1 - Draw the scenary/background
+//create the background/scenary
 function scenary() {
   push();
   noStroke();
+  background(255, 255, 255);
 
-  //draw the sky
+  //the sky
   fill(200, 255, 255);
   rect(0, 0, width, height);
 
-  //draw the ground
+  //the sun
+  fill(255, 255, 0);
+  ellipse(780, 50, 200);
+
+  //the ground
   fill(10, 170, 10);
-  rect(0, 400, width, height);
+  rect(0, 550, width, height);
   pop();
 }
 
-//step 2 - Draw the obstacles
-function obstacles(x, y) {
+function obstacle1(x, y) {
   push();
-  fill(55, 55, 55);
-  rect(500, 300, 100, 100);
+  translate(x, y);
+
+  //obstacle # 1 - a house
+  noStroke();
+
+  //base house
+  fill(200, 55, 55);
+  rect(0, 0, 230, 150);
+
+  //door
+  fill(155, 120, 80);
+  rect(150, 70, 50, 80);
+  fill(0, 0, 0);
+  ellipse(160, 110, 10);
+
+  //window
+  fill(255, 185, 0);
+  rect(40, 40, 50, 50);
+  stroke(0, 0, 0);
+  strokeWeight(3);
+  line(65, 40, 65, 90);
+  line(40, 65, 90, 65);
+
   pop();
 }
 
-//step 3 - Draw the vehicle
+//draw the vehicle
 function vehicle(x, y) {
   push();
   translate(x, y);
 
   //color of the vehicle
   fill(255, 0, 0);
+
   //top of the vehicle
   rect(30, -30, 40, 40, 10);
 
@@ -58,11 +100,13 @@ function vehicle(x, y) {
   fill(0, 0, 0);
   ellipse(10, 50, 20);
   ellipse(90, 50, 20);
+
   pop();
 }
 
+// step # - create the draw function to make the canvas and the content visable
 function draw() {
   scenary();
-  obstacles(obstaclesX, 150);
-  vehicle(100, vehicleY);
+  obstacle1(600, 430);
+  vehicle(80, 200);
 }
--- readme.md
+++ readme.md
@@ -2,7 +2,7 @@
 
 ### Rules
 
-- Vehicle can move up and down
+- Vehicle can move up and down with up-arrow || spacebar on keyboard
 - Obstacles move to the left
 - Vehicle falls down to the ground because of gravity
 - Vehicle goes up when clicking the up-key or spacebar
@@ -10,30 +10,20 @@
 
 #### Order to build
 
-1. Draw the background
-2. Draw the obstacles
-3. Draw the vehicle
-4. Add gravity to the vehicle
-5. Make the vehicle fly
-6. Detect collision between ground and vehicle.
-7. ...
+1. Create the canvas
+2. Create an array for the values
 
-#### Step 0
+   - Create an object containing the different values for x, y, velocity, acceleration, etc.
+   - Create an object containing the key-arrow/spacebar values
+   - Create a variable boolean --> Game is running - true/false for an if-statement
 
-Set up the canvas
+3. Draw the background (with a function)
+4. Draw the obstacles (with a function for each obstacle)
 
-#### Step 1
+   - Create x# of obstacles (use an array?)?
 
-Draw the scenery/background
-
-#### Step 2
-
-Draw the obstacles
-
-#### Step 3
-
-Draw the vehicle. Don't forget the x, y parameters for movement.
-
-#### Step 4
-
-Make the vehicle
+5. Draw the vehicle (with a function)
+6. Add gravity to the vehicle
+7. Make the vehicle fly
+8. Detect collision between ground and vehicle
+9. Detect collision between the obstacle and vehicle
